6a8f2d0adcc496ab4b2623db9a0c09999fbc1d01
Marianne <marianne.metais@grenoble-inp.org>
2022-09-09 09:48:19 +0200

    change launcher

--- launchers/jz/train_brouhaha.sh
+++ launchers/jz/train_brouhaha.sh
@@ -10,5 +10,5 @@ python main.py runs/brouhaha/ train \
 -p Brouhaha.SpeakerDiarization.NoisySpeakerDiarization \
 --classes brouhaha \
 --model_type pyannet \
---epoch 10 \
---data_dir "/gpfsscratch/rech/xdz/uzm31mf/data_new/mini_data"
+--epoch 100 \
+--data_dir "/gpfsscratch/rech/xdz/uzm31mf/final_dataset/NoisyDataset"
